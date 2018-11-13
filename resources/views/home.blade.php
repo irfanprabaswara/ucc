fb68b5e3ccfe0c63bda098081f7f70172b3749bc
irfan <irfanprabaswara@gmail.com>
2018-11-13 10:46:38 +0700

    semua file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,82 @@
-@extends('layouts.app')
+@extends('layouts.templateMahasiswa')
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <!-- FIRST ROW OF BLOCKS -->     
+      <div class="row">
 
-                <div class="panel-body">
-                    You are logged in!
+      <!-- USER PROFILE BLOCK -->
+        <div class="col-sm-3 col-lg-3">
+            <div class="dash-unit">
+                <dtitle>User Profile</dtitle>
+                <hr>
+                <div class="thumbnail">
+                    <img src="images/face80x80.jpg" alt="{{ Auth::user()->name }}" class="img-circle">
+                </div><!-- /thumbnail -->
+                <h1>Marcel Newman</h1>
+                <h3>Madrid, Spain</h3>
+                <br>
+                    <div class="info-user">
+                        <span aria-hidden="true" class="li_user fs1"></span>
+                        <span aria-hidden="true" class="li_settings fs1"></span>
+                        <span aria-hidden="true" class="li_mail fs1"></span>
+                        <span aria-hidden="true" class="li_key fs1"></span>
+                    </div>
                 </div>
+        </div>
+
+      <!-- DONUT CHART BLOCK -->
+        <div class="col-sm-3 col-lg-3">
+            <div class="dash-unit">
+                <dtitle>Site Bandwidth</dtitle>
+                <hr>
+                <div id="load"></div>
+                <h2>45%</h2>
             </div>
         </div>
-    </div>
+
+      <!-- DONUT CHART BLOCK -->
+        <div class="col-sm-3 col-lg-3">
+            <div class="dash-unit">
+                <dtitle>Disk Space</dtitle>
+                <hr>
+                <div id="space"></div>
+                <h2>65%</h2>
+            </div>
+        </div>
+        
+        <div class="col-sm-3 col-lg-3">
+
+      <!-- LOCAL TIME BLOCK -->
+            <div class="half-unit">
+                <dtitle>Local Time</dtitle>
+                <hr>
+                    <div class="clockcenter">
+                        <digiclock>12:45:25</digiclock>
+                    </div>
+            </div>
+
+      <!-- SERVER UPTIME -->
+            <div class="half-unit">
+                <dtitle>Server Uptime</dtitle>
+                <hr>
+                <div class="cont">
+                    <p><img src="images/up.png" alt=""> <bold>Up</bold> | 356ms.</p>
+                </div>
+            </div>
+
+        </div>
+      </div><!-- /row -->
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Dashboard</div>
+
+                        <div class="panel-body">
+                            You are logged in!
+                        </div>
+                    </div>
+                </div>
+            </div>
 </div>
 @endsection
---
+++ resources/views/layouts/templateMahasiswa.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <title>{{ $title }}</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="{{ $meta['description'] }}">
+    <meta name="author" content="{{ $meta['author'] }}">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="{{asset('plugins/bootstrap/css/bootstrap.min.css')}}">
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="{{asset('plugins/font-awesome/css/font-awesome.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/font-awesome/css/icomoon.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/flexslider/flexslider.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/pretty-photo/css/prettyPhoto.css')}}">
+    <!-- Theme CSS -->
+    <link id="theme-style" rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <link id="theme-style" rel="stylesheet" href="{{asset('css/custom-style.css')}}">
+    @if($active == 'kuisioner')
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style-form.css')}}">
+    @endif
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- [if lt IE 9]> -->
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <!-- <![endif] -->
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
