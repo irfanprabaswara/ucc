9fba0908cc8cfce5b6614f4a569c8a1a86b38682
abudriman <abudriman@gmail.com>
2018-11-20 21:28:42 +0700

    fix layouting index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,26 +22,23 @@
     <!-- jumbotron end -->
     <!-- about -->
     <section class="overlapped-navbar-fixed" id="onepage-about">
-    <div class="row">
-        <div class="col-xs-12 col-md-8 col-md-offset-2">
+    <div class="row" style="padding-bottom:40px;    ">
+        <div class="col-xs-12 col-md-8 col-md-offset-2" >
             <h1 class="text-center">About Us</h1>
             <hr class="layouting-fix">
-            <div class="col-xs-12 col-md-6 ">
+            <div class="col-xs-12 col-md-6 " >
                 <p class="text-justify">&nbsp;&nbsp;&nbsp;&nbspBerkembangnya teknologi informasi di era milenial saat ini menyebabkan peningkatan kompetensi SDM di segala bidang sangat diperlukan. Pasar kerja tidak hanya membutuhkan SDM yang handal, tangguh dan berkualitas tetapi juga peka dan mampu mengelola perubahan.</p>
                 <p class="text-justify">&nbsp;&nbsp;&nbsp;&nbspUntuk mengantisipasi persaingan SDM di era global ini, diperlukan pengembangan riset di bidang sumber daya manusia . Hasil riset dan publikasi ilmiah yang berfokus pada karir dan pengembangan kapasitas individu sangat diperlukan untuk memberikan rekomendasi secara kontinyu dan signiﬁkan dalam rangka meningkatkan daya saing bangsa.</p>
                 <p class="text-justify">&nbsp;&nbsp;&nbsp;&nbspTanpa riset, akan sangat sulit bagi sebuah organisasi untuk menentukan dan mengembangkan target pasar serta potensi bisnis mereka. Selain itu, kurangnya riset juga mengakibatkan banyak organisasi yang mengeluhkan adanya kesenjangan kompetensi antara kebutuhan perusahaan dan sumber daya yang tersedia. Diponegoro Research Center (DIRECT) merupakan divisi dari Diponegoro Human Development Center (DHDC) yang bertugas memfasilitasi penyediaan jasa riset untuk mewadahi urgensi kebutuhan terkait pengembangan Sumber Daya Manusia (SDM).</p>
             </div>
-            <div class="col-xs-12 col-md-6" style="padding-top:30px">
+            <div class="col-xs-12 col-md-6">
+            <p class="text-justify">&nbsp;&nbsp;&nbsp;&nbspDIRECT hadir untuk membantu organisasi memenuhi kebutuhannya terkait pengembangan SDM, baik organisasi bisnis, swasta, pendidikan dan pemerintahan. Hasil dari riset ini akan digunakan sebagai dasar dalam penyusunan dan pengembangan program organisasi yang bergerak di sumber daya manusia, sehingga dapat memberikan kontribusi terbaik dalam Pengembangan SDM di Indonesia.</p>
                 <img src="{{asset('images/ucc-direct/direktur.png')}}" alt="direktur direct" class="img-rounded center-block" style="width:70%">
                 <h4>Director : <strong>Mirwan Surya Perdhana, S.E., MM., Ph.</strong></h4>
             </div>
         </div>
-        <div class="col-xs-12 col-md-8 col-md-offset-2" style="padding-bottom:30px">
-            <div class="col-xs-12">
-            <p class="text-justify">&nbsp;&nbsp;&nbsp;&nbspDIRECT hadir untuk membantu organisasi memenuhi kebutuhannya terkait pengembangan SDM, baik organisasi bisnis, swasta, pendidikan dan pemerintahan. Hasil dari riset ini akan digunakan sebagai dasar dalam penyusunan dan pengembangan program organisasi yang bergerak di sumber daya manusia, sehingga dapat memberikan kontribusi terbaik dalam Pengembangan SDM di Indonesia.</p>
-            </div>
-        </div>
     </div>
+    <br><br>
     </section>
     <!-- about end -->
     
@@ -57,7 +54,7 @@
     </div>
     <section class="overlapped-navbar-fixed" id="onepage-programsandservices">
     </section>
-    <div class="row" style="padding-bottom:150px">
+    <div class="row" style="padding-bottom:150px;padding-top:20px">
         <div class="col-xs-12 col-md-8 col-md-offset-2">
             <h1 class="text-center">Programs & Services</h1>
             <hr class="layouting-fix">
